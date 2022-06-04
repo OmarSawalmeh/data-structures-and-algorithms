f469771c83817c12e0783a23113650be688516fc
Omar Sawalmeh <sawalmehomar10@gmail.com>
2022-06-04 14:45:16 +0300

    Linked List Data Structures

--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -1,10 +1,72 @@
 'use strict';
 
 // Require our linked list implementation
-const LinkedLilst = require('../index');
+const LinkedList = require('../LinkedList');
 
 describe('Linked List', () => {
-  it('works', () => {
-    expect(true).toBeTruthy();
+
+  test("Create instantiate an empty linked list", ()=>{
+    let list = new LinkedList();
+    expect(list.head).toBeNull();
+  });
+
+  test('Insert into the linked list', ()=>{
+    let list = new LinkedList();
+    list.insert(11);
+    list.insert(9)
+    list.insert(7);
+    expect(list.head.value).toEqual(7);
+  });
+
+  test('The head property will properly point to the first node in the linked list', ()=>{
+    let list = new LinkedList();
+    list.append(50);
+    list.append(60);
+    list.append(80);
+    expect(list.head.value).toEqual(50);
+  });
+
+  test('Insert multiple nodes into the linked list', ()=>{
+    let list = new LinkedList();
+    list.insert(11);
+    list.insert(9)
+    list.insert(7);
+    expect(list.head.value).toEqual(7);
+    expect(list.head.next.value).toEqual(9);
+    expect(list.head.next.next.value).toEqual(11);
+  });
+
+  test('Check if a Value send exists as a Node’s value somewhere within the list', ()=>{
+    let list = new LinkedList();
+    list.append(60);
+    list.append(80);
+    list.insert(11);
+    list.insert(9)
+    list.insert(7);
+    expect(list.includes(60)).toEqual(true);
+    expect(list.includes(11)).toEqual(true);
+    expect(list.includes(9)).toEqual(true);
+  });
+
+  test('Check if a Value send NOT exists as a Node’s value somewhere within the list', ()=>{
+    let list = new LinkedList();
+    list.append(60);
+    list.append(80);
+    list.insert(11);
+    list.insert(9)
+    list.insert(7);
+    expect(list.includes(100)).toEqual(false);
+    expect(list.includes(55)).toEqual(false);
+    expect(list.includes(0)).toEqual(false);
+  });
+
+  test('Return a collection of all the values that exist in the linked list', ()=>{
+    let list = new LinkedList();
+    list.append(60);
+    list.append(80);
+    list.insert(11);
+    list.insert(9)
+    list.insert(7);
+    expect(list.toString()).toEqual(`${7} -> ${9} -> ${11} -> ${60} -> ${80} -> NULL`);
   });
 });
--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,7 +1,26 @@
 'use strict';
 
-class LinkedList {
+const LinkedList = require('./LinkedList');
 
-}
+const ll = new LinkedList();
+ll.append(7);
+ll.append(9);
+ll.append(11);
+ll.append(15);
+ll.append(24);
+ll.append(28);
+ll.append(33);
 
-module.exports = LinkedList;
+// Insert a new Node To The Head of Linked List.....
+ll.insert(5);
+ll.insert(0);
+
+// Check if a Value send exists as a Node’s value somewhere within the list......
+console.log(ll.includes(0)); // -----> return true
+console.log(ll.includes(33)); // -----> return true
+console.log(ll.includes(37)); // -----> return false
+
+// Return String representing all the values in the Linked Lis
+console.log(ll.toString());
+
+ll.printAll();
